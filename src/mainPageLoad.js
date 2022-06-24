ef2a5ff4d6f808c592356acbf557c676d034074d
Arthur <a-murk@mail.ee>
2022-06-24 16:28:02 +0300

    Wrap links in anchor tags

--- src/mainPageLoad.js
+++ src/mainPageLoad.js
@@ -7,7 +7,9 @@ export default function() {
     const linkList = createElement('ul', 'list');
     const copyright = createElement('small', undefined, '© Põhjamaa Köök');
     for (let i = 0; i < linkItems.length; i++) {
-        const link = createElement('li', undefined, linkItems[i]);
+        const link = createElement('li');
+        const anchor = createElement('a', undefined, linkItems[i]);
+        link.append(anchor);
         linkList.append(link);
     }
     const main = createElement('main');
