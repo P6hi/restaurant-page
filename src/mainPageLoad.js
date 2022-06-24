10afd4237fe720be6768e24afe765a0adcf7a56f
Arthur <a-murk@mail.ee>
2022-06-24 16:18:05 +0300

    Create page load module
    Function creates the main content layout of the page

---
+++ src/mainPageLoad.js
@@ -0,0 +1,21 @@
+import createElement from './elementCreate.js';
+
+export default function() {
+    const linkItems = ['Home', 'Menu', 'Contact'];
+    const header = createElement('header');
+    const companyName = createElement('h1', 'company-name', 'Põhjamaa Köök');
+    const linkList = createElement('ul', 'list');
+    const copyright = createElement('small', undefined, '© Põhjamaa Köök');
+    for (let i = 0; i < linkItems.length; i++) {
+        const link = createElement('li', undefined, linkItems[i]);
+        linkList.append(link);
+    }
+    const main = createElement('main');
+    const footer = createElement('footer');
+    header.append(companyName);
+    header.append(linkList);
+    footer.append(copyright);
+    document.body.append(header);
+    document.body.append(main);
+    document.body.append(footer);
+}
